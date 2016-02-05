71f1e4e4bfa684c33f23e7c550c177c3c08bc840
Console <howdy@iconsole.tv>
2016-02-04 17:43:44 -0800

    Francis

--- data/sounds/AudioPackage12.mk
+++ data/sounds/AudioPackage12.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := frameworks/base/data/sounds
 
 # Simple files that do not require renaming
 ALARM_FILES := Argon Carbon Helium Krypton Neon Oxygen Osmium Platinum
-NOTIFICATION_FILES := Ariel Ceres Carme Elara Europa Iapetus Io Rhea Salacia Titan Tethys
+NOTIFICATION_FILES := Ariel Ceres Carme Elara Europa Francis Iapetus Io Rhea Salacia Titan Tethys
 RINGTONE_FILES := Callisto Dione Ganymede Luna Oberon Phobos Sedna Titania Triton Umbriel
 EFFECT_FILES := Effect_Tick KeypressReturn KeypressInvalid KeypressDelete KeypressSpacebar KeypressStandard \
 	camera_focus Dock Undock Lock Unlock Trusted
